1b7cd26f57a8681bdd2274111aae46a476247af8
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-09-20 02:32:07 +0530

    Update stats.py

--- HuTao/modules/stats.py
+++ HuTao/modules/stats.py
@@ -1,20 +1,13 @@
-import HuTao.Config
-import platform
-import time
-import datetime
 from HuTao import app, dbname
-from HuTao.database.users_db import *
-from pyrogram import filters , Client
-from platform import python_version
-from HuTao.Config import SUDO 
-from psutil import boot_time, cpu_percent, disk_usage, virtual_memory
-from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
-from motor.motor_asyncio import AsyncIOMotorClient
+from pyrogram import filters
 from pyrogram.types import Message
+from HuTao.database.users_db import Users  # Import the Users class
+
+usrdb = dbname["users"]
 
 @app.on_message(filters.command("stats") & filters.private)
 async def get_stats(_, message: Message):
-    total_users = await Users.count_users()
+    total_users = await Users.count_users(usrdb)  # Pass the database instance to count_users
 
     stats_message = f"**Total Users: {total_users}**"
 
@@ -22,6 +15,6 @@ async def get_stats(_, message: Message):
 
 class Users:
     @staticmethod
-    async def count_users():
-        count = await usrdb.count_documents({})
+    async def count_users(db):
+        count = await db.count_documents({})  # Count the user documents in the database
         return count
