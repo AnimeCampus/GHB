38b4d4abd174aa7537725b573cd8d33d6706747f
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-09-20 02:36:06 +0530

    Update stats.py

--- HuTao/modules/stats.py
+++ HuTao/modules/stats.py
@@ -1,13 +1,20 @@
 from HuTao import app, dbname
 from pyrogram import filters
 from pyrogram.types import Message
-from HuTao.database.users_db import Users  # Import the Users class
+from HuTao.database.users_db import Users
+from HuTao.Config import OWNER, SUDO
 
 usrdb = dbname["users"]
 
-@app.on_message(filters.command("stats") & filters.private)
+@app.on_message(filters.command(["stats", "users"]))
 async def get_stats(_, message: Message):
-    total_users = await Users.count_users(usrdb)  # Pass the database instance to count_users
+    user_id = message.from_user.id
+
+    if user_id not in SUDO_USERS and user_id != OWNER_ID:
+        await message.reply_text("You are not authorized to use this command.")
+        return
+
+    total_users = await Users.count_users(usrdb)
 
     stats_message = f"**Total Users: {total_users}**"
 
@@ -16,5 +23,5 @@ async def get_stats(_, message: Message):
 class Users:
     @staticmethod
     async def count_users(db):
-        count = await db.count_documents({})  # Count the user documents in the database
+        count = await db.count_documents({})
         return count
