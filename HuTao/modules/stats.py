7a935886e7bc3667f1a458edc18789f0aa80dec9
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-09-19 19:50:17 +0530

    Update stats.py

--- HuTao/modules/stats.py
+++ HuTao/modules/stats.py
@@ -2,7 +2,7 @@ import HuTao.Config
 import platform
 import time
 import datetime
-from HuTao import app, dbname
+from HuTao import app, dbname, usrdb
 from pyrogram import filters , Client
 from platform import python_version
 from HuTao.Config import SUDO 
