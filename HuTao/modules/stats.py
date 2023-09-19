2f5b13f1965c52bd5286733daee29ab1147641fb
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-09-19 19:46:52 +0530

    Update stats.py

--- HuTao/modules/stats.py
+++ HuTao/modules/stats.py
@@ -5,17 +5,17 @@ import datetime
 from HuTao import app, dbname
 from pyrogram import filters , Client
 from platform import python_version
-from HuTao.Config import SUDO as SUPREME_USERS
+from HuTao.Config import SUDO 
 from psutil import boot_time, cpu_percent, disk_usage, virtual_memory
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 
 
 @Client.on_message(filters.command('stats'))
 async def _stats(client,message):
-     if message.from_user.id not in SUPREME_USERS:
+     if message.from_user.id not in SUDO:
           return
      status = "** 「 Bot Statistics: 」**\n\n"
-     status += f'**• Total Chats :** `{await db.chats.count_documents({})}`\n'
-     status += f'**• Total Users :** `{await db.users.count_documents({})}`\n'   
+     status += f'**• Total Chats :** `{await dbname.chats.count_documents({})}`\n'
+     status += f'**• Total Users :** `{await dbname.users.count_documents({})}`\n'   
      await message.reply_text(status,reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("❌ Close",callback_data='close')]]))
     
